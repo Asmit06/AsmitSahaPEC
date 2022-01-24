68d55107e14df21f0032034cbb45a94bdd8002ef
Asmit Saha <89724163+Asmit06@users.noreply.github.com>
2022-01-24 19:34:06 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,94 @@
+#question 1
+str1="Python is a case sensitive language"
+#part a
+str1_len=len(str1)
+print("Length of input string is :",str1_len) #length of string
+#part b
+str_rev=str1[::-1] #reverses the string
+print("Reverse of input string is :",str_rev) 
+#part c
+str_cop=str1[10:26] #slices the string
+print(str_cop) 
+#part d
+str3=str1.replace("a case sensitive","object oriented") #replaces "a case sensitive" with "object orieted"
+print(str3)
+#part e
+str1_index=str1.index(" a ")+1 #gives first occurence of "a"
+print(str1_index)
+#part f
+str1_white=str1.strip() #removes whitespace from string
+print(str1_white)
+print()
+print("----------------------------------------------------------------------------------------")
+print()
+print()
+
+#Question 2
+name=input("Enter name:")
+sid=input("Enter SID:")
+dept=input("Enter Department Name:")
+cgpa=float(input("Enter CGPA:"))
+print("Hey,",name,"Here! \nMy SID is",sid," \nI am from",dept,"department and my CGPA is",cgpa) #\n is used to move pointer to new line when printing
+print()
+print("----------------------------------------------------------------------------------------")
+print()
+print()
+
+#Question 3
+a=56
+b=10
+print("a&b :",a&b)#bitwise AND
+print("a|b :",a|b)#bitwise OR
+print("a^b :",a^b)#bitwise XOR
+print("a<<2 :",a<<2)#bitwise left shift 2 bits
+print("b<<2 :",b<<2)#bitwise left shift 2 bits
+print("a>>2 :",a>>2)#bitwise right shift 2 bits
+print("b>>4 :",b>>4)#bitwise right shift 4 bits
+print()
+print("----------------------------------------------------------------------------------------")
+print()
+print()
+
+#Question 4
+a1=int(input("Enter a number :"))
+b1=int(input("Enter a number :"))
+c1=int(input("Enter a number :"))
+if a1>b1:# checks if a1 is greater than b1
+    if a1>c1:# checks if a1 is greater than c1
+        print(a1,"is greatest")
+    else:
+        print(c1,"is greatest")
+else:
+    if b1>c1:# checks if b1 is greater than c1
+        print(b1,"is greatest")
+    else:
+        print(c1,"is greatest")
+print()
+print("----------------------------------------------------------------------------------------")
+print()
+print()
+
+#Question 5
+org_str=input("Enter the string: ")
+flag=org_str.find("name")#searches for index of "name" and returns -1 if not found
+if flag==-1:
+    print("No")
+else:
+    print("Yes")
+print()
+print("----------------------------------------------------------------------------------------")
+print()
+print()
+
+#Question 6
+s1=int(input("Enter length of one side of triangle:"))
+s2=int(input("Enter length of one side of triangle:"))
+s3=int(input("Enter length of one side of triangle:"))
+if s1+s2>s3 and s2+s3>s1 and s3+s1>s2: #checks if triangle is valid or not
+    print("Yes")
+else:
+    print("No")
+print()
+print("----------------------------------------------------------------------------------------")
+print()
+print()
---
+++ output2.py
@@ -0,0 +1,59 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/sahab/OneDrive/Desktop/test/firs.py ============
+Length of input string is : 35
+Reverse of input string is : egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Python is a case sensitive language
+
+----------------------------------------------------------------------------------------
+
+
+Enter name:Asmit
+Enter SID:21104105
+Enter Department Name:Electrical
+Enter CGPA:9.7
+Hey, Asmit Here! 
+My SID is 21104105  
+I am from Electrical department and my CGPA is 9.7
+
+----------------------------------------------------------------------------------------
+
+
+a&b : 8
+a|b : 58
+a^b : 50
+a<<2 : 224
+b<<2 : 40
+a>>2 : 14
+b>>4 : 0
+
+----------------------------------------------------------------------------------------
+
+
+Enter a number :3
+Enter a number :4
+Enter a number :5
+5 is greatest
+
+----------------------------------------------------------------------------------------
+
+
+Enter the string: Please enter your name
+Yes
+
+----------------------------------------------------------------------------------------
+
+
+Enter length of one side of triangle:3
+Enter length of one side of triangle:4
+Enter length of one side of triangle:5
+Yes
+
+----------------------------------------------------------------------------------------
+
+
+>>> 
